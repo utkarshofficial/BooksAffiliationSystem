84c1e5d3832776ac42043c8a3a4aaa1197361c8c
Utkarsh Sharma <utkarshofficialin@gmial.com>
2022-10-29 10:12:12 +0530

    removed json old functions

--- script.js
+++ script.js
@@ -34,38 +34,10 @@ document.querySelectorAll(".drop").forEach((drop) => {
     getBooksData();
   });
 });
-// json get funciton
-function get() {
-  fetch("books.json")
-    .then((res) => res.json())
-    .then((res) => {
-      showBooks(res);
-    });
-}
 
-// show books of selected category
-function showBooks(data) {
-  DATA = data;
-  let bookSelection = localStorage.getItem("currentBookSelection");
-  // for create instance of books from json
-  let book = "";
-  for (let j = 0; j < data.length; j++) {
-    if (data[j].type === bookSelection) {
-      for (let i = 0; i < data[j].books.length; i++) {
-        book += `<section class="book-box">
-                <span class="temp-title">Title</span>
-                <span class="book-name">${data[j].books[i].title}</span>
-                <img class="book-cover" src="${data[j].books[i].cover}" width="170" height="200">
-                <a class="buy-btn" href="${data[j].books[i].buylink}" alt="buy link">Buy Now</a>
-                </section>`;
-      }
-    }
-  }
-  document.querySelector("#books").innerHTML = book;
-}
-// get();
+
+
 // getting full docs of books
-
 // show books of selected category
 async function showList(Data) {
   let data = Data;
@@ -82,6 +54,7 @@ async function showList(Data) {
   document.querySelector("#books").innerHTML = book;
 }
 
+// if category changed then change the book data
 async function getBooksData() {
   const incomeSnap = await getDoc(doc(db, "bookData", "income"));
   const mindSnap = await getDoc(doc(db, "bookData", "mind"));
